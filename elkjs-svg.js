3fbd1e4255b05a2a535011edc6d9ad1185d0dd59
Emil Stenström <em@kth.se>
2020-08-04 18:11:18 +0200

    Strip trailing spaces.

--- elkjs-svg.js
+++ elkjs-svg.js
@@ -5,34 +5,34 @@ const styles = require("./src/styles.js");
 const defs = require("./src/defs.js");
 
 function Renderer() {
-  // configuration 
+  // configuration
   this._style = styles.__defaults.map(s => styles[s]).join("\n");
   this._defs = defs.__defaults.map(d => defs[d]).join("\n");
-  
+
   this.reset();
 }
 
 Renderer.prototype = {
   constructor: Renderer,
-  
+
   reset() {
-    // internal housekeeping  
+    // internal housekeeping
     this._edgeRoutingStyle = {
       __global: "POLYLINE"
     };
     this._parentIds = {};
-    this._edgeParents = {};  
+    this._edgeParents = {};
   },
-      
+
   init(root) {
     // reset
     this.reset();
     this.registerParentIds(root);
     this.registerEdges(root);
   },
- 
+
   /* Utility methods. */
-  
+
   // edges can be specified anywhere, there coordinates however are relative
   //  a) to the source node's parent
   //  b) the source node, if the target is a descendant of the source node
@@ -53,11 +53,11 @@ Renderer.prototype = {
     }
     if (e.id) {
       return e.id;
-    } 
+    }
     var suffix = id.substr(id.lastIndexOf('.') + 1);
     if (e[suffix]) {
       return e[suffix];
-    } 
+    }
     return undefined;
   },
 
@@ -87,9 +87,9 @@ Renderer.prototype = {
   },
 
   /*
-   * Rendering methods. 
+   * Rendering methods.
    */
-  
+
   renderRoot(root) {
     var s = this.svgHead(root);
     s += "<defs>\n";
@@ -107,7 +107,7 @@ Renderer.prototype = {
     var s = "";
     s += "<g transform=\"translate(" + (graph.x || 0) + "," + (graph.y || 0) +")\">";
     s += "\n";
-    // paint edges first such that ports are drawn on top of them 
+    // paint edges first such that ports are drawn on top of them
     s += (this._edgeParents[graph.id] || []).map((e) => { return this.renderEdge(e, graph); }).join("\n");
     s += "\n";
     s += (graph.children || []).map(c => this.renderNode(c)).join("\n");
@@ -212,13 +212,13 @@ Renderer.prototype = {
   },
 
   bendsToSpline(cps) {
-    
+
     let s = '';
     if (cps.length) {
       let {x, y} = cps[0];
       s += `M${x} ${y} `;
     }
-    
+
     var i = 1;
     while (i < cps.length) {
       var left = cps.length - i;
@@ -307,29 +307,29 @@ Renderer.prototype = {
     }
     return s;
   },
-  
-  
+
+
   /*
    * Public API
    */
-   
+
   styles(...styles) {
-    if (styles.length == 0) 
+    if (styles.length == 0)
       return this._style;
     this._style = styles.join(" ");
-    return this;  
-  },
-  
-  defs(...defs) {
-    if (defs.length == 0) 
-      return this._defs;
-    this._defs = defs.join(" "); 
     return this;
   },
-  
+
+  defs(...defs) {
+    if (defs.length == 0)
+      return this._defs;
+    this._defs = defs.join(" ");
+    return this;
+  },
+
   /**
-   * the options that have been used during layout. 
-   * They are used here to derive edge routing. 
+   * the options that have been used during layout.
+   * They are used here to derive edge routing.
    */
   usedLayoutOptions(opts) {
     var edgeRouting = this.getOption(opts, layoutOptions.edgeRouting);
@@ -338,7 +338,7 @@ Renderer.prototype = {
     }
     return this;
   },
-  
+
   toSvg(json) {
    this.init(json);
    return this.renderRoot(json);
